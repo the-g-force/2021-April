3ebc23875051bfe4ae91c003b9edeb65672b357f
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-04-21 16:47:20 -0400

    Encode the idea of correctness and correct guessing

--- project/src/GuessingGame.gd
+++ project/src/GuessingGame.gd
@@ -23,13 +23,38 @@ const FLOWERS = [
 	},
 ]
 
+onready var _button_grid := $ButtonGrid
+
 func _ready():
 	var options = FLOWERS
 	$TextureRect.texture = options[0].image
+	
+	# The fact that the first of the four buttons is correct
+	# is currently hard-coded here.
 	for i in range(0,4):
 		var b := Button.new()
+		b.set_script(preload("res://src/GuessButton.gd"))
 		b.text = options[i].name
 		b.size_flags_horizontal = SIZE_EXPAND_FILL
 		b.size_flags_vertical = SIZE_EXPAND_FILL
+		b.correct = i == 0
+		b.connect("pressed", self, "_on_GuessButton_pressed", [b], CONNECT_ONESHOT)
 		$ButtonGrid.add_child(b)
-		
+
+
+func _on_GuessButton_pressed(button:Button):
+	for b in _button_grid.get_children():
+		b.disabled = true
+	
+	if button.correct:
+		_show_success()
+	else:
+		_show_failure()
+
+
+func _show_success():
+	print("Success")
+	
+
+func _show_failure():
+	print("Failure")
