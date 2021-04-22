e88ffa45adfcb8cd9a97d5c33a19995ae2d4bfe9
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-04-22 15:09:05 -0400

    Demonstrate the use of a frame that animates pages

--- project/src/GameFrame.gd
+++ project/src/GameFrame.gd
@@ -2,7 +2,14 @@ extends Control
 
 
 func _on_GuessingGame_next():
+	$AnimationPlayer.get_animation("FlyOut").track_set_path(0, "CanvasLayer:offset")
+	$AnimationPlayer.play("FlyOut")
+	yield($AnimationPlayer, "animation_finished")
+	
 	var next_round = preload("res://src/GuessingGame.tscn").instance()
-	remove_child(get_child(0))
-	add_child(next_round)
+	var canvas_layer = CanvasLayer.new()
+	canvas_layer.add_child(next_round)
+	remove_child($CanvasLayer)
+	canvas_layer.name = "CanvasLayer"
 	next_round.connect("next", self, "_on_GuessingGame_next")
+	add_child(canvas_layer)
